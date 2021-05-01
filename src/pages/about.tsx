1334027fbf1c2275067accac53ed6c82ae0e834b
viniciusoliveiras <vinitag190@gmail.com>
2021-05-01 18:07:13 -0300

    useEffect on alerts

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,12 +1,16 @@
 import Head from 'next/head';
+import { useEffect } from 'react';
 import { Skeleton } from '@chakra-ui/react';
 
 import { Header } from '../components/Header';
 
 export default function About() {
-  alert(
-    '⚠️ Página ainda em construção. Alguns recursos podem não estar disponíveis ou otimizados. ⚠️'
-  );
+  useEffect(() => {
+    alert(
+      '⚠️ Página ainda em construção. Alguns recursos podem não estar disponíveis ou otimizados. ⚠️'
+    );
+  }, []);
+
   return (
     <>
       <Head>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import Head from 'next/head';
+import { useEffect } from 'react';
 import { Header } from '../components/Header';
 
 export default function Home() {
-  alert(
-    '⚠️ Página ainda em construção. Alguns recursos podem não estar disponíveis ou otimizados. ⚠️'
-  );
+  useEffect(() => {
+    alert(
+      '⚠️ Página ainda em construção. Alguns recursos podem não estar disponíveis ou otimizados. ⚠️'
+    );
+  }, []);
+
   return (
     <>
       <Head>
