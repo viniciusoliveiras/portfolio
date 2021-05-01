9005cdb8bd73d62d2d2040345871700f42116890
viniciusoliveiras <vinitag190@gmail.com>
2021-05-01 18:02:49 -0300

    rename pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
 import Head from 'next/head';
+import { Header } from '../components/Header';
 
-export default function Start() {
+export default function Home() {
+  alert(
+    '⚠️ Página ainda em construção. Alguns recursos podem não estar disponíveis ou otimizados. ⚠️'
+  );
   return (
     <>
       <Head>
         <title>Vinicius Rei Delas</title>
       </Head>
+
+      <Header />
     </>
   );
 }
