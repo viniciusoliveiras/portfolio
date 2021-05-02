376537a3fcf8b7a13fe9b7777906df4b7db9b981
viniciusoliveiras <vinitag190@gmail.com>
2021-05-02 14:19:01 -0300

    404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,56 @@
+import { Box, Flex, Heading, Link, Text } from '@chakra-ui/react';
+import React from 'react';
+import { Header } from '../components/Header';
+
+export default function Custom404() {
+  return (
+    <>
+      <Header />
+      <Flex
+        align='center'
+        justify='center'
+        mt={{ xs: '4', md: '10', lg: '12' }}
+        flexDirection='column'
+        p='6'
+      >
+        <Heading fontSize={{ xs: '7xl', md: '8xl' }} textAlign='center'>
+          😳
+        </Heading>
+
+        <Heading fontSize={{ xs: '5xl', md: '7xl' }} mt='6' textAlign='center'>
+          Whoops!
+        </Heading>
+
+        <Text fontSize={{ xs: '2xl', md: '3xl' }} mt='12' textAlign='center'>
+          404 | Página não encontrada
+        </Text>
+
+        <Text fontSize={{ xs: 'xl', md: '2xl' }} mt='12' textAlign='center'>
+          Parece que esta página é tímida e não quer aparecer no portfólio
+        </Text>
+
+        <Link
+          href='/home'
+          textDecoration='none'
+          w='min'
+          h='3rem'
+          color='white'
+          mt='20'
+        >
+          <Box
+            as='button'
+            bgColor='red.400'
+            w='max'
+            p='2'
+            fontWeight='medium'
+            fontSize='lg'
+            borderRadius='4'
+            _hover={{ transition: 0.2, filter: 'brightness(0.9)' }}
+          >
+            Voltar para o Início
+          </Box>
+        </Link>
+      </Flex>
+    </>
+  );
+}
