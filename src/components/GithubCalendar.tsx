84597c3b70ce77a51f816c0155fd5929707c4556
viniciusoliveiras <vinitag190@gmail.com>
2021-05-22 16:52:53 -0300

    About page - GithubCalendar mobile

--- src/components/GithubCalendar.tsx
+++ src/components/GithubCalendar.tsx
@@ -1,31 +1,52 @@
 import React from 'react';
 import GitHubCalendar from 'react-github-calendar';
 import ReactTooltip from 'react-tooltip';
-import { Flex, Text } from '@chakra-ui/react';
+import { Flex, Text, useBreakpointValue } from '@chakra-ui/react';
 
 export function GithubCalendar() {
+  const isWideVersion = useBreakpointValue({
+    xs: false,
+    md: true,
+  });
+
   return (
     <>
       <Flex flexDirection='column' mt='14'>
-        <Text fontSize={{ sm: 'xl', xs: '2xl', md: '3xl' }} color='red.400'>
+        <Text fontSize={{ xs: '2xl', md: '3xl' }} color='red.400'>
           Contribuições no Github
         </Text>
 
-        <Flex
-          flexDirection='column'
-          mt='6'
-          align='center'
-        >
-          <GitHubCalendar
-            username='viniciusoliveiras'
-            color='#00A8CC' //  #FF6464
-            blockSize={20}
-            blockMargin={5}
-            fontSize={18}
+        {isWideVersion && (
+          <Flex flexDirection='column' mt='6' align='center' bgColor='aqua'>
+            <GitHubCalendar
+              username='viniciusoliveiras'
+              color='#00A8CC' //  #FF6464
+              blockSize={20}
+              blockMargin={5}
+              fontSize={18}
+            >
+              <ReactTooltip delayShow={50} html />
+            </GitHubCalendar>
+          </Flex>
+        )}
+
+        {!isWideVersion && (
+          <Flex
+            flexDirection='column'
+            mt='4'
+            align='center'
           >
-            <ReactTooltip delayShow={50} html />
-          </GitHubCalendar>
-        </Flex>
+            <GitHubCalendar
+              username='viniciusoliveiras'
+              color='#00A8CC' //  #FF6464
+              blockSize={30}
+              blockMargin={5}
+              fontSize={16}
+            >
+              <ReactTooltip delayShow={50} html />
+            </GitHubCalendar>
+          </Flex>
+        )}
       </Flex>
     </>
   );
--- src/components/KnowWhoIAm.tsx
+++ src/components/KnowWhoIAm.tsx
@@ -98,7 +98,7 @@ export function KnowWhoIAm({ bio }: KnowWhoIAmProps) {
 
           <TechAndToolStacks />
 
-          {/* <GithubCalendar /> */}
+          <GithubCalendar />
 
           <Footer />
         </Flex>
