d42be54cbec43ee6beb21b1413581888ac100378
viniciusoliveiras <vinitag190@gmail.com>
2021-05-22 15:09:56 -0300

    Ferramentas que utilizo

--- src/components/KnowWhoIAm.tsx
+++ src/components/KnowWhoIAm.tsx
@@ -5,11 +5,7 @@ import { Tech } from './Tech';
 export function KnowWhoIAm() {
   return (
     <>
-      <Flex
-        mt='14'
-        mx={{ xs: '10', sm: '10', md: '10', lg: '16', xl: '32' }}
-        flexDirection='column'
-      >
+      <Flex mt='14' mx={{ xs: '12', xl: '16' }} flexDirection='column'>
         <Flex align='center' justify='space-around' flex='1'>
           <Flex>
             <Tilt>
@@ -40,29 +36,56 @@ export function KnowWhoIAm() {
           </Flex>
         </Flex>
 
-        <Text
-          fontSize={{ sm: 'xl', xs: '2xl', md: '3xl' }}
-          mt='16'
-          color='red.400'
-        >
-          Tecnologias que utilizo
-        </Text>
+        <Grid templateColumns='repeat(2, 1fr)' gap={{ xs: 4, lg: 6, xl: 12 }}>
+          <GridItem>
+            <Text
+              fontSize={{ sm: 'xl', xs: '2xl', md: '3xl' }}
+              mt='16'
+              color='red.400'
+            >
+              Tech Stack
+            </Text>
 
-        <Flex>
-          <Grid
-            templateColumns='repeat(3, 1fr)'
-            mt='4'
-            gap={{ xs: 4, lg: 6, xl: 12 }}
-            p='6'
-          >
-            <Tech imageSRC='https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-plain.svg' />
-            <Tech imageSRC='https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg' />
-            <Tech imageSRC='https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg' />
-            <Tech imageSRC='https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg' />
-            <Tech imageSRC='https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg' />
-            <Tech imageSRC='https://raw.githubusercontent.com/devicons/devicon/master/icons/nextjs/nextjs-original.svg' />
-          </Grid>
-        </Flex>
+            <Flex>
+              <Grid
+                templateColumns='repeat(3, 1fr)'
+                mt='4'
+                gap={{ xs: 4, lg: 6, xl: 12 }}
+                p={{ xl: 6 }}
+              >
+                <Tech imageSRC='https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-plain.svg' />
+                <Tech imageSRC='https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg' />
+                <Tech imageSRC='https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg' />
+                <Tech imageSRC='https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg' />
+                <Tech imageSRC='https://raw.githubusercontent.com/devicons/devicon/master/icons/nextjs/nextjs-original.svg' />
+                <Tech imageSRC='https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg' />
+              </Grid>
+            </Flex>
+          </GridItem>
+
+          <GridItem>
+            <Text
+              fontSize={{ sm: 'xl', xs: '2xl', md: '3xl' }}
+              mt='16'
+              color='red.400'
+            >
+              Ferramentas que utilizo
+            </Text>
+
+            <Flex>
+              <Grid
+                templateColumns='repeat(3, 1fr)'
+                mt='4'
+                gap={{ xs: 4, lg: 6, xl: 12 }}
+                p={{ xl: 6 }}
+              >
+                <Tech imageSRC='https://raw.githubusercontent.com/devicons/devicon/master/icons/ubuntu/ubuntu-plain.svg' />
+                <Tech imageSRC='https://raw.githubusercontent.com/devicons/devicon/master/icons/visualstudio/visualstudio-plain.svg' />
+                <Tech imageSRC='images/vercel-icon-dark.svg' />
+              </Grid>
+            </Flex>
+          </GridItem>
+        </Grid>
       </Flex>
     </>
   );
