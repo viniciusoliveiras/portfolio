21a7dbb7d6c94a50141e78d6c5e7299f5497d2fd
viniciusoliveiras <vinitag190@gmail.com>
2021-08-19 17:37:11 -0300

    perf: ⚡️ add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
+// });
+
+// module.exports = withBundleAnalyzer({});
+
 module.exports = {
   async redirects() {
     return [
